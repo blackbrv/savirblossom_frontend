00450714ba3ce2024328479ef5644a1aefc28efd
Blackbrv <luthfiprtm@gmail.com>
2026-03-21 20:23:11 +0700

    Update: App layout and config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
-import AosProviders from "@/components/AosProviders";
+import AppProviders from "@/contexts/AppProviders";
 import Footer from "@/components/ui/Footer";
 
 const geistSans = Geist({
@@ -31,11 +31,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AosProviders>
+        <AppProviders>
           <Navbar />
           {children}
           <Footer />
-        </AosProviders>
+        </AppProviders>
       </body>
     </html>
   );
